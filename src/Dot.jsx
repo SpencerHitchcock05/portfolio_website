5bebab0565e1241bc41db4c13d6454e583b061b4
Spencer <sp797499@dal.ca>
2025-01-06 10:48:42 -0400

    added sorting project and portfolio project, adjusted dots animation

--- src/Dot.jsx
+++ src/Dot.jsx
@@ -8,6 +8,7 @@ function Dot(props) {
 
     const radius = useRef();
     const dot = useRef();
+    const mouseDistance = useRef();
     const [dotCoords, setDotCoords] = useState([0,0]);
 
     const data = props.data
@@ -20,8 +21,8 @@ function Dot(props) {
                 const rect = cur.getBoundingClientRect();
                 const mouseX = e.clientX - rect.left - 50; 
                 const mouseY = e.clientY - rect.top - 50;
-                const mouseDistance = Math.sqrt(Math.pow(mouseX, 2) + Math.pow(mouseY, 2));
-                if (mouseDistance < 150) {
+                mouseDistance.current = Math.sqrt(Math.pow(mouseX, 2) + Math.pow(mouseY, 2));
+                if (mouseDistance.current < 150) {
 
             
 
@@ -49,7 +50,7 @@ function Dot(props) {
     return (
         <>
             <div ref={radius} style={{left: `${data.x}vw`, top: `${data.y}vh`}} className="dot-radius">
-                <div ref={dot} style={{transform: `translate(${dotCoords[0]}px, ${dotCoords[1]}px)`}} className="dot"></div>
+                <div ref={dot} style={{transform: `translate(${dotCoords[0]}px, ${dotCoords[1]}px) scale(${mouseDistance.current < 150? 1 + ( 150 - Math.sqrt(Math.pow(dotCoords[0], 2) + Math.pow(dotCoords[1], 2))) / 150 : 1 })`}} className="dot"></div>
             </div>
         </>
     )
